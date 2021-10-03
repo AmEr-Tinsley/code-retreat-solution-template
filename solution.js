a45f351e74d00ca9acd7376cc5e4538456c7bccc
AmEr-Tinsley <amerhosni07@gmail.com>
2021-10-03 12:45:06 +0200

    intialize

---
+++ solution.js
@@ -0,0 +1,26 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    
+    // here goes your code
+}
